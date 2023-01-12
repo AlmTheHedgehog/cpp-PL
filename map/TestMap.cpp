4465c810fedba2abbe5ae1d58d585e4eb7f996aa
AlmTheHedgehog <tbe6666@gmail.com>
2023-01-12 09:50:03 +0100

    Map. Separated main and more try blocks

--- map/TestMap.cpp
+++ map/TestMap.cpp
@@ -17,54 +17,73 @@ void modifyEmployees(Database& database);
 void addBooks(BooksDB& booksSh);
 void modifyBooks(BooksDB& booksSh);
 
+void checkEmpDB(Database& database);
+void checkBooksDB(BooksDB& booksSh);
+
 int main() {
 	Database database;
+	checkEmpDB(database);
+
+	BooksDB newDB;
+	checkBooksDB(newDB);
+};
+
+void checkEmpDB(Database& database){
 	addEmployees(database);
 
 	Database newDatabase = database;
 	try{									// Make a copy of database
 		newDatabase.add(830505432, Employee("Ewa Nowak", "charwoman", 43));	// Add fourth employee
 	}catch(ElementAlreadyExistsException&){
-		cout << "element already exicts" << endl;
+		cout << "element 830505432 already exicts" << endl;
 	}
 
-	try{									// Make a copy of database
-		modifyEmployees(newDatabase);
-	}catch(ElementAlreadyExistsException&){
-		cout << "some of adding func in modifyEmployees return unhandelled exception" << endl;
-	}
+	modifyEmployees(newDatabase);
 	
-
 	cout << "Original database:" << endl << database << endl;
 	cout << "Modified database:" << endl << newDatabase << endl;
 
 	database = newDatabase;												// Update original database
 
 	cout << "Database after the assignment:" << endl << database << endl;
+}
 
-	BooksDB newDB;
-	cout << "New DB with main numbers:" << endl << newDB << endl;
+void checkBooksDB(BooksDB& booksSh){
+	
+	cout << "New DB with main numbers:" << endl << booksSh << endl;
 
 	try{									// Make a copy of database
-		addBooks(newDB);
+		addBooks(booksSh);
 	}catch(ElementAlreadyExistsException&){
 		cout << "some of adding func in modifyEmployees return unhandelled exception" << endl;
 	}
 
 	try{
-		modifyBooks(newDB);
+		modifyBooks(booksSh);
 	}catch(ElementAlreadyExistsException&){
 		cout << "some of elements exist" << endl;
 	}
 
-	cout << "Modified database:" << endl << newDB << endl;
-	
-};
+	cout << "Modified database:" << endl << booksSh << endl;
+}
 
 void addEmployees(Database& database) {
-	database.add(761028073, Employee("Jan Kowalski", "salesman", 28)); 	// Add first employee: name: Jan Kowalski, position: salseman, age: 28,
-	database.add(510212881, Employee("Adam Nowak", "storekeeper", 54));	// Add second employee
-	database.add(730505129, Employee("Anna Zaradna", "secretary", 32));	// Add third employee
+	try{
+		database.add(761028073, Employee("Jan Kowalski", "salesman", 28)); 	// Add first employee: name: Jan Kowalski, position: salseman, age: 28,
+	}catch(ElementAlreadyExistsException&){
+		cout << "761028073 - EXISTS ALREADY" << endl;
+	}
+	try{
+		database.add(510212881, Employee("Adam Nowak", "storekeeper", 54));	// Add second employee
+	}catch(ElementAlreadyExistsException&){
+		cout << "510212881 - EXISTS ALREADY" << endl;
+	}
+	try{
+		database.add(730505129, Employee("Anna Zaradna", "secretary", 32));	// Add third employee
+	}catch(ElementAlreadyExistsException&){
+		cout << "730505129 - EXISTS ALREADY" << endl;
+	}
+
 	try{
 		database.add(730505129, Employee("Anna Zaradna", "secretary", 32));	// Add third employee
 	}catch(ElementAlreadyExistsException&){
@@ -91,9 +110,21 @@ void modifyEmployees(Database& database) {
 }
 
 void addBooks(BooksDB& booksSh){
-	booksSh.add(345687654, Book("Runner", "New Author", "Horror", 52, false ));
-	booksSh.add(324855547, Book("The bed is bad", "Bob Dealan", "Strange", 265, true ));
-	booksSh.add(545874518, Book("My journey", "Jiin", "Searcher", 85, true ));
+	try{
+		booksSh.add(345687654, Book("Runner", "New Author", "Horror", 52, false ));
+	}catch(ElementAlreadyExistsException&){
+		cout << "345687654 - EXISTS ALREADY" << endl;
+	}
+	try{
+		booksSh.add(324855547, Book("The bed is bad", "Bob Dealan", "Strange", 265, true ));
+	}catch(ElementAlreadyExistsException&){
+		cout << "345687654 - EXISTS ALREADY" << endl;
+	}
+	try{
+		booksSh.add(545874518, Book("My journey", "Jiin", "Searcher", 85, true ));
+	}catch(ElementAlreadyExistsException&){
+		cout << "345687654 - EXISTS ALREADY" << endl;
+	}
 }
 
 void modifyBooks(BooksDB& booksSh){
